65b7883b0fc38f15587d1c65db160f4661f9918f
Paul Mojica <Paul.Mojica@theremigroup.com>
2020-02-01 16:06:25 -0500

    get clients works

--- GetClients/index.ts
+++ GetClients/index.ts
@@ -3,20 +3,25 @@ import { MongoService } from '../shared/services/mongo.service';
 import { Client, ClientSession } from '../shared/models/clients.interface';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<Client[]> {
-
+    let mongoSvc: MongoService = null;
     try {
-        const mongoSvc = new MongoService();
+        mongoSvc = new MongoService();
         const clients: Client[] = await mongoSvc.getCollection('Clients');
         const clientSessions: ClientSession[] = await mongoSvc.getCollection('ClientSessions');
         const clientsWithSessions = clients.map(client => {
             const currentClientSessions = clientSessions.filter(session => client.ClientID === session.ClientID);
             return { ...client, ClientSessions: currentClientSessions };
         });
-        return Promise.resolve(clientsWithSessions);
+        context.res = {
+            status: 200,
+            body: clientsWithSessions
+        };
+        
     } catch (ex) {
         return Promise.reject(ex);
     } finally {
-        this._dataSvc.disconnect();
+        mongoSvc.disconnect();
+        context.done();
     }
 };
 
