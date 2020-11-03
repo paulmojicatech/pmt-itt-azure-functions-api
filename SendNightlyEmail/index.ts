b7257317ffe047ffba64986145ea411cdc6a3b1a
Paul Mojica <paulmojicatech@gmail.com>
2020-11-02 19:02:02 -0500

    fix time for daylight savings time

--- SendNightlyEmail/index.ts
+++ SendNightlyEmail/index.ts
@@ -154,8 +154,8 @@ const timerTrigger: AzureFunction = async function (
         );
 
         const offset = process?.env?.PLATFORM === 'Azure' ?
-            utc.getHours() - 4 :
-            utc.getHours() + 4;
+            utc.getHours() - 5 :
+            utc.getHours() + 5;
         const timeZoneDate = new Date(utc.setHours(offset)); 
         const hour = timeZoneDate.getHours();
         
@@ -229,55 +229,6 @@ const timerTrigger: AzureFunction = async function (
     context.done();
   }
 
-  // var timeStamp = new Date().toISOString();
-  // let mongoSvc: MongoService;
-  // try {
-  //     mongoSvc = new MongoService();
-  //     if (myTimer.IsPastDue) {
-  //         const sessionWhere = {
-  //             $and: [
-  //                 {
-  //                     ClientSessionDate: { $gte: "2019-12-01" }
-  //                 },
-  //                 {
-  //                     ClientSessionDate: { $gte: "2020-01-01" }
-  //                 },
-  //                 {
-  //                     ClientID: 2
-  //                 }
-  //             ]
-  //         };
-
-  //         // const clientSessions = await mongoSvc.getCollection('ClientSessions', sessionWhere);
-  //         // const clientWhere = {
-  //         //     $or: [
-  //         //         {
-  //         //             ClientEmail: { $nin: [null, ""]}
-  //         //         },
-  //         //         {
-  //         //             ClientSecondaryEmail: { $nin: [null, ""]}
-  //         //         }
-  //         //     ]
-  //         // };
-  //         // const clients = await mongoSvc.getCollection('Clients', clientWhere);
-  //         // const emailsToSend = chain(clientSessions)
-  //         //                         .filter(item => item.)
-  //     }
-  //     context.res = {
-  //         status: 200,
-  //         body: 'Emails sent'
-  //     };
-  // } catch (ex) {
-  //     context.res = {
-  //         status: 500,
-  //         body: ex
-  //     };
-  // } finally {
-  //     if (!!mongoSvc) {
-  //         mongoSvc.disconnect();
-  //     }
-  //     context.done();
-  // }
 };
 
 export default timerTrigger;
